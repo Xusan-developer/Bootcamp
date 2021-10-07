5012306bd215515a1cd2fa85c12eccab1279d001
Husan19981111 <husanboriyev@gmail.com>
2021-10-07 16:10:55 +0500

    Lab 2 qo'shildi

---
+++ README.md
@@ -0,0 +1,3 @@
+# Solutions to Bootcamp tasks
+-Lab 2
+-Lab 3
---
+++ Solutions/Lab1.cs
@@ -0,0 +1,13 @@
+namespace bootcamp.Solutions
+{
+    public class Lab1
+    {
+        public void Problem1()
+        {
+            System.Console.WriteLine("222222");
+            System.Console.WriteLine("2    2");
+            System.Console.WriteLine("2    2");
+            System.Console.WriteLine("222222");
+        }
+    }
+}
